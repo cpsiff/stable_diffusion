b58c93c0883c4030e9746e340334ad2dbe235f45
Carter Sifferman <cpsiff@gmail.com>
2022-11-15 19:07:57 -0600

    resizing

--- ablation.py
+++ ablation.py
@@ -7,6 +7,8 @@ import os
 
 from diffusers import StableDiffusionImg2ImgPipeline
 
+RESIZE = 512
+
 device = "cuda"
 model_path = "CompVis/stable-diffusion-v1-4"
 
@@ -25,6 +27,13 @@ generator = torch.Generator(device=device).manual_seed(1024)
 for fold_name in os.listdir("corrupted_imgs"):
     for img_name in os.listdir(os.path.join("corrupted_imgs", fold_name)):
         init_img = Image.open(os.path.join("corrupted_imgs", fold_name, img_name))
+        
+        if RESIZE:
+            if init_img.size[0] > init_img.size[1]:
+                init_img = init_img.resize((RESIZE, int(init_img.size[1]*(RESIZE/init_img.size[0]))))
+            else:
+                init_img = init_img.resize((int(init_img.size[0]*(RESIZE/init_img.size[1])), RESIZE))
+
         for strength in np.linspace(0.1, 0.9, 3):
             for guidance_scale in np.linspace(1, 9, 3):
                 with autocast("cuda"):
