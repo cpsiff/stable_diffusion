18b259c865ac2c83af69d497a0233f37e90ecd17
Carter Sifferman <cpsiff@gmail.com>
2022-11-16 09:17:56 -0600

    better saving

--- ablation.py
+++ ablation.py
@@ -4,10 +4,22 @@ from tqdm.auto import tqdm
 from PIL import Image
 import numpy as np
 import os
+import yaml
 
 from diffusers import StableDiffusionImg2ImgPipeline
 
+FOLDS = "JPEG", "PALETTE"
 RESIZE = 512
+STRENGTHS = [0.1]
+GUIDANCE_SCALES = [0.5, 1]
+SEED = 100
+OUTPUT_DIR = "output"
+
+PROMPTS = [
+    "4K, high definition, crisp desktop background",
+    "4K, high definition, crisp",
+    "high definition deblurred denoised",
+]
 
 device = "cuda"
 model_path = "CompVis/stable-diffusion-v1-4"
@@ -20,11 +32,11 @@ pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
 )
 pipe = pipe.to(device)
 
-prompt = "A fantasy landscape, trending on artstation"
+generator = torch.Generator(device=device).manual_seed(SEED)
 
-generator = torch.Generator(device=device).manual_seed(1024)
+index = {} #store an index of prompts used for different file names
 
-for fold_name in os.listdir("corrupted_imgs"):
+for fold_name in FOLDS:
     for img_name in os.listdir(os.path.join("corrupted_imgs", fold_name)):
         init_img = Image.open(os.path.join("corrupted_imgs", fold_name, img_name))
         
@@ -34,11 +46,30 @@ for fold_name in os.listdir("corrupted_imgs"):
             else:
                 init_img = init_img.resize((int(init_img.size[0]*(RESIZE/init_img.size[1])), RESIZE))
 
-        for strength in np.linspace(0.1, 0.9, 3):
-            for guidance_scale in np.linspace(1, 9, 3):
-                with autocast("cuda"):
-                    image = pipe(prompt=prompt, init_image=init_img, strength=strength, guidance_scale=guidance_scale, generator=generator).images[0]
-                    # image = init_img
-                    save_name = f"output/{fold_name}_{img_name.split('.')[0]}_s{str(strength).replace('.', 'dot')}_gs{str(guidance_scale).replace('.', 'dot')}.png"
-                    print(save_name)
-                    image.save(save_name)
+        for strength in STRENGTHS:
+            for guidance_scale in GUIDANCE_SCALES:
+                for prompt, i in zip(PROMPTS, range(len(PROMPTS))):  
+                    with autocast("cuda"):
+                        image = pipe(
+                            prompt=prompt,
+                            init_image=init_img,
+                            strength=strength,
+                            guidance_scale=guidance_scale,
+                            generator=generator
+                        ).images[0]
+                        # image = init_img
+                        save_name = f"{OUTPUT_DIR}/{fold_name}_{img_name.split('.')[0]}_s{str(strength).replace('.', 'dot')}_gs{str(guidance_scale).replace('.', 'dot')}_{i}.png"
+                        index[save_name] = {
+                            "prompt": prompt,
+                            "guidance_scale": guidance_scale,
+                            "strength": strength,
+                            "fold_name": fold_name,
+                            "img_name": img_name,
+                            "seed": SEED,
+                            "resize": RESIZE
+                        }
+                        print(save_name)
+                        image.save(save_name)
+
+with open(f"{OUTPUT_DIR}/index.yaml") as f:
+    yaml.dump(index, f)
