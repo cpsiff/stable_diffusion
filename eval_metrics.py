3cdf04395a0fd2d8b4296219e46a2b57c91177b0
Carter Sifferman <cpsiff@gmail.com>
2022-12-01 15:31:51 -0600

    remove test folder

--- eval_metrics.py
+++ eval_metrics.py
@@ -1,7 +1,7 @@
 import yaml
 import pandas as pd
 
-INDEX_FILE = "/home/carter/repos/stable-diffusion/test/output/20221201-205305/index.yaml"
+INDEX_FILE = "index.yaml"
 
 # load metrics in from yaml file
 with open(INDEX_FILE) as f:
